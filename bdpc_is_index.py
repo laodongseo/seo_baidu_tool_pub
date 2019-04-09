b99f8046c95a39f84309d5525cdeb87893f14fdb
dong <772483200@qq.com>
2019-04-09 12:21:50 +0800

    a

--- bdpc_is_index.py
+++ bdpc_is_index.py
@@ -3,7 +3,7 @@
 准备url.txt,一行一个url,必须带http或https
 区分https或者http
 区分https://aaa/bbb和https://aaa/bbb/
-查询某个url是否收录，有收录再访问该url获取title，查询通过搜索title该url是否有排名
+查询某个url是否收录，有收录再访问该url获取title(模拟百度蜘蛛UA获取title)，查询通过搜索title该url是否有排名
 """
 
 import requests
@@ -27,21 +27,21 @@ class BdpcIndex(threading.Thread):
         return q
 
     # 获取某待查询url的title
-    def get_title(self, url):
-        html = self.get_html(url)
+    def get_title(self, url, user_agent):
+        html = self.get_html(url, user_agent)
         if html:
             doc = pq(html)
             title = doc('title').text()
             return title
 
     # 获取某待查询url或某词的serp源码
-    def get_html(self, url, retry=2):
+    def get_html(self, url, user_agent, retry=2):
         try:
             r = requests.get(url=url, headers=user_agent, timeout=5)
         except Exception as e:
             print('获取源码失败', url, e)
             if retry > 0:
-                self.get_html(url, retry - 1)
+                self.get_html(url, user_agent, retry - 1)
         else:
             html = r.text
             return html
@@ -93,30 +93,33 @@ class BdpcIndex(threading.Thread):
     def run(self):
         while 1:
             target_url = q.get()
-            print('查询', target_url)
             # 查询该target_url是否收录
             url = "https://www.baidu.com/s?ie=utf-8&wd={0}".format(target_url)
-            html = self.get_html(url)
+            html = self.get_html(url,user_agent)
             encrypt_url_list = self.get_encrpt_urls(html)
             real_urls = self.get_real_urls(encrypt_url_list)
             num_target_url = self.check_include(target_url, real_urls)
             # 有收录则判断是否索引
             if num_target_url == 1:
                 # 查询该target_url的title 检查是否有索引
-                title = self.get_title(target_url)
-                url = "https://www.baidu.com/s?ie=utf-8&wd={0}".format(title)
-                html = self.get_html(url)
-                encrypt_url_list = self.get_encrpt_urls(html)
-                real_urls = self.get_real_urls(encrypt_url_list)
-                num_title = self.check_include(target_url, real_urls)
-                if num_title == 1:
-                    print("收录且索引")
-                    f.write(target_url+'\t'+'收录且索引\n')
-                elif num_title == 0:
-                    print("收录无索引")
-                    f.write(target_url + '\t' + '收录无索引\n')
+                title = self.get_title(target_url, baidu_ua)
+                if title:
+                    url = "https://www.baidu.com/s?ie=utf-8&wd={0}".format(title)
+                    html = self.get_html(url, user_agent)
+                    encrypt_url_list = self.get_encrpt_urls(html)
+                    real_urls = self.get_real_urls(encrypt_url_list)
+                    num_title = self.check_include(target_url, real_urls)
+                    if num_title == 1:
+                        print(target_url, "收录且索引")
+                        f.write(target_url+'\t'+'收录且索引\n')
+                    elif num_title == 0:
+                        print(target_url, "收录无索引")
+                        f.write(target_url + '\t' + '收录无索引\n')
+                else:
+                    print(target_url, '未获取title')
+                    f.write(target_url + '\t' + '未获取title\n')
             elif num_target_url == 0:
-                print("无收录")
+                print(target_url, "无收录")
                 f.write(target_url + '\t' + '无收录\n')
             q.task_done()
 
@@ -125,6 +128,7 @@ if __name__ == "__main__":
 
     user_agent = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'}
+    baidu_ua = {'User-Agent': 'Mozilla/5.0 (compatible; Baiduspider/2.0; +http://www.baidu.com/search/spider.html'}
     q = BdpcIndex.read_txt('url.txt')
     f = open('bdpc_index.txt','w',encoding='utf-8')
     # 设置线程数

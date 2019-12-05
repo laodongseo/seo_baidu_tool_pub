741a27dc4a9f74edee91a99fb260a4e1d8ed0c5c
董董 <772483200@qq.com>
2019-12-05 17:20:06 +0800

    Update bdpc1_page5.py

--- bdpc1_page5.py
+++ bdpc1_page5.py
@@ -1,9 +1,12 @@
 # ‐*‐ coding: utf‐8 ‐*‐
 """
+监控某域名前五页排名变化，可实现分关键词类别监控
 bdpc1_page5_info.txt记录每个kwd在第几页有排名
 bdpc1_page5_rankurl.txt记录每个kwd前五页排名的url，当前页面出现多个就记录多个
 生成的excel和txt是首页 前二/三/四/五/五页后的数量
 bdpc1_page5_info.txt的行数就是真正查询成功的词数
+excel文件kwd_core_city.xlsx 中每个sheet名代表一类关键词,每个sheet第一列放关键词
+现在百度PC反爬有点严重,head头要加cookie,查询频繁还要轮换cookie
 """
 import requests
 from pyquery import PyQuery as pq
@@ -16,6 +19,56 @@ from openpyxl import Workbook
 import time
 import gc
 
+
+# 计算最终结果
+def get_result(file_path, result, result_last):
+    # 读取文件 计算首页到五页每页词数
+    for line in open(file_path, 'r', encoding='utf-8'):
+        line = line.strip().split('\t')
+        page = line[1]
+        group = line[2]
+        result[group][page] += 1
+
+    # 计算首页、前二页、前三页、前四页、前五页词数
+    for group, page_num in result.items():
+        print(group, page_num)
+        for page, num in page_num.items():
+            if page == '五页后':
+                result_last[group]['五页后'] = num
+            if page in ['首页', '二页', '三页', '四页', '五页']:
+                result_last[group]['前五页'] += num
+            if page in ['首页', '二页', '三页', '四页']:
+                result_last[group]['前四页'] += num
+            if page in ['首页', '二页', '三页']:
+                result_last[group]['前三页'] += num
+            if page in ['首页', '二页']:
+                result_last[group]['前二页'] += num
+            if page == '首页':
+                result_last[group]['首页'] += num
+    return result_last
+
+
+# 写入excel文件
+def write_excel(group_list, result_last, today):
+    wb = Workbook()
+    # 创建sheet
+    for group in group_list:
+        sheet_num = 0
+        wb.create_sheet(u'{0}'.format(group), index=sheet_num)
+        sheet_num += 1
+    # 写内容
+    row_first = ['日期', '首页', '前二页', '前三页', '前四页', '前五页', '五页后']
+    for group, data_dict in result_last.items():
+        # 写第一行表头
+        wb[group].append(row_first)
+        # 写数据
+        row_value = [today]
+        for page, value in data_dict.items():
+            row_value.append(value)
+        wb[u'{0}'.format(group)].append(row_value)
+    wb.save('{0}bdpc1_page5.xlsx'.format(today))
+
+
 class bdpcCoverPage5(threading.Thread):
 
     def __init__(self):
@@ -25,29 +78,48 @@ class bdpcCoverPage5(threading.Thread):
     def read_excel(filepath):
         q = queue.Queue()
         group_list = []
+        kwd_dict = {}
         wb_kwd = load_workbook(filepath)
         for sheet_obj in wb_kwd:
             sheet_name = sheet_obj.title
             group_list.append(sheet_name)
+            kwd_dict[sheet_name]= []
             col_a = sheet_obj['A']
             for cell in col_a:
                 kwd = (cell.value)
                 # 加个判断,防止一些不可见字符
                 if kwd:
-                    q.put([kwd,sheet_name])
+                    kwd_z = kwd+ '租房'
+                    kwd_er = kwd + '二手房'
+                    q.put([sheet_name,kwd_z])
+                    q.put([sheet_name,kwd_er])
         return q, group_list
 
     # 初始化结果字典
     @staticmethod
     def result_init(group_list):
-        result = {}
+        pages = ['首页','二页','三页','四页','五页','五页后']
+        result = {} #记录首页、第二页..第五页各多少词
         for group in group_list:
             result[group] = {}
         for group in group_list:
-            for page in page_list:
+            for page in pages:
                 result[group][page] = 0
         print("结果字典init...")
         return result
+
+    # 初始化最后结果字典
+    @staticmethod
+    def result_last_init(group_list):
+        result_last = {}  # 记录首页、第二页..第五页各多少词
+        pages = ['首页','前二页','前三页','前四页','前五页','五页后']
+        for group in group_list:
+            result_last[group] = {}
+        for group in group_list:
+            for page in pages:
+                result_last[group][page] = 0
+        print("最终结果字典init...")
+        return result_last
 
     # 获取某词serp源码
     def get_html(self,url,retry=2):
@@ -55,18 +127,20 @@ class bdpcCoverPage5(threading.Thread):
             r = requests.get(url=url,headers=user_agent,timeout=5)
         except Exception as e:
             print('获取源码失败',e)
-            time.sleep(1)
+            time.sleep(6)
             if retry > 0:
                 self.get_html(url,retry-1)
         else:
-            html = r.content.decode('utf-8')
-            return html
+            html = r.content.decode('utf-8',errors='ignore')  # 用r.text有时候识别错误
+            url = r.url
+            return html,url
 
     # 获取某词serp源码上自然排名的所有url
-    def get_encrpt_urls(self,html):
+    def get_encrpt_urls(self,html,url):
         encrypt_url_list = []
-        if html and '_百度搜索' in html:
-            doc = pq(html)
+        doc = pq(html)
+        title = doc('title').text()
+        if '_百度搜索' in title and 'https://www.baidu.com/s?tn=48020221' in url:
             try:
                 a_list = doc('.t a').items()
             except Exception as e:
@@ -77,42 +151,49 @@ class bdpcCoverPage5(threading.Thread):
                     if encrypt_url.find('http://www.baidu.com/link?url=') == 0:
                         encrypt_url_list.append(encrypt_url)
         else:
-            print('结果页有问题,可能被反爬')
+            print(title,'源码异常,可能反爬')
+            time.sleep(6)
         return encrypt_url_list
 
     # 解密某条加密url
     def decrypt_url(self,encrypt_url,retry=1):
+        real_url = None # 默认None
         try:
             encrypt_url = encrypt_url.replace('http://','https://')
             # print(encrypt_url)
             r = requests.head(encrypt_url,headers=user_agent)
         except Exception as e:
             print(encrypt_url,'解密失败',e)
+            time.sleep(6)
             if retry > 0:
                 self.decrypt_url(encrypt_url,retry-1)
         else:
-            return r.headers['Location']
+            real_url = r.headers['Location']
+            return real_url
 
     # 获取某词serp源码首页排名真实url
     def get_real_urls(self,encrypt_url_list):
         real_url_list = [self.decrypt_url(encrypt_url) for encrypt_url in encrypt_url_list]
+        real_url_set = set(real_url_list)
+        real_url_set = real_url_set.remove(None) if None in real_url_set else real_url_set
+        real_url_list = list(real_url_set)
         return real_url_list
 
-    # 提取某条url域名部分
+    # 提取某url的域名部分
     def get_domain(self,real_url):
+        domain = None
         try:
            res = urlparse(real_url)
         except Exception as e:
            print (e,real_url)
-           domain = "xxx"
         else:
            domain = res.netloc
         return domain
 
-    # 获取某词serp源码首页排名真实url的域名部分
+    # 获取某词serp源码首页排名所有域名
     def get_domains(self,real_url_list):
             domain_list = [self.get_domain(real_url) for real_url in real_url_list]
-            # 搜一个词 同一个域名多个url出现排名 只计算一次
+            # 一个词某域名多个url有排名,计算一次
             domain_set = set(domain_list)
             domain_set = domain_set.remove(None) if None in domain_set else domain_set
             domain_str = ','.join(domain_set)
@@ -120,18 +201,18 @@ class bdpcCoverPage5(threading.Thread):
 
     # 线程函数
     def run(self):
-        global success_num
         while 1:
-            kwd_group = q.get()
-            kwd,group = kwd_group
+            group_kwd = q.get()
+            group,kwd = group_kwd
             try:
                 for page in page_dict.keys():
                     if page == '':
-                        url = "https://www.baidu.com/s?ie=utf-8&wd={0}".format(kwd)
+                        url = "https://www.baidu.com/s?tn=48020221_28_hao_pg&ie=utf-8&wd={0}".format(kwd)
                     else:
-                        url = "https://www.baidu.com/s?ie=utf-8&wd={0}&pn={1}".format(kwd,page)
-                    html = self.get_html(url)
-                    encrypt_url_list = self.get_encrpt_urls(html)
+                        url = "https://www.baidu.com/s?tn=48020221_28_hao_pg&ie=utf-8&wd={0}&pn={1}".format(kwd,page)
+                    # print(url)
+                    html,now_url = self.get_html(url)
+                    encrypt_url_list = self.get_encrpt_urls(html,now_url)
                     real_url_list = self.get_real_urls(encrypt_url_list)
                     # print(real_url_list)
                     domain_str = self.get_domains(real_url_list)
@@ -146,95 +227,49 @@ class bdpcCoverPage5(threading.Thread):
                         f.write('{0}\t{1}\t{2}\n'.format(kwd,'五页后',group))
                 f.flush()
                 f_url.flush()
-                threadLock.acquire()
-                success_num += 1
-                threadLock.release()
-                print(success_num)
             except Exception as e:
                 print(e)
             finally:
                 del kwd
-                del group
                 gc.collect()
                 q.task_done()
 
 
 if __name__ == "__main__":
-    start = time.time() 
+    start = time.time()
     local_time = time.localtime()
     today = time.strftime('%Y%m%d',local_time)
     domain = '5i5j.com'
     user_agent = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 SE 2.X MetaSr 1.0'}
-    threadLock = threading.Lock()  # 锁
-    success_num = 0  # 查询成功个数
-    page_list = ['首页','二页','三页','四页','五页','五页后'] #查询页码 全局变量
-    q,group_list = bdpcCoverPage5.read_excel('./kwd_core_city.xlsx') #关键词队列及属性
-    result = bdpcCoverPage5.result_init(group_list) #结果字典
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36',
+        'Host': 'www.baidu.com',
+        'Referer': 'https://www.hao123.com/?tn=48020221_28_hao_pg',
+        'Cookie':'BDUSS=RQSWpXeEd5a1lDLXF6OExrVmdzeW9WUnFqRWtTNGRpWEctOU1RSmw0bHB5WU5aSVFBQUFBJCQAAAAAAAAAAAEAAADlnXKtt7CwybK-vbYAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAGk8XFlpPFxZeG'
+    }
+    q,group_list = bdpcCoverPage5.read_excel('kwd_core_city.xlsx')  # 关键词队列及分类
+    result = bdpcCoverPage5.result_init(group_list)  # 结果字典-记录首页到5页各多少词
+    result_last = bdpcCoverPage5.result_last_init(group_list)  # 结果字典-统计首页,前2页到前5页多少词
     all_num = q.qsize() # 总词数
-    page_dict = {'':'首页',10:'二页',20:'三页',30:'四页',40:'五页'} #查询页数
+    page_dict = {'':'首页',10:'二页',20:'三页',30:'四页',40:'五页'}  # 查询页码
     f = open('{0}bdpc1_page5_info.txt'.format(today),'w',encoding="utf-8")
     f_url = open('{0}bdpc1_page5_rankurl.txt'.format(today),'w',encoding="utf-8")
+    file_path = f.name
     # 设置线程数
-    for i in list(range(3)):
+    for i in list(range(1)):
         t = bdpcCoverPage5()
         t.setDaemon(True)
         t.start()
     q.join()
     f.close()
     f_url.close()
-
-    # 读取文件 计算首页到五页【每页】的词数
-    for line in open('{0}bdpc1_page5_info.txt'.format(today),'r',encoding='utf-8'):
-        line = line.strip().split('\t')
-        page = line[1]
-        group = line[2]
-        result[group][page] += 1
-
-    print(result)
-    # 计算首页、前二页、前三页、前四页、前五页词数
-    for group,page_num in result.items():
-        for page,num in page_num.items():
-            if page in ['五页后']:
-                result[group]['五页后'] = num
-        for page,num in page_num.items():
-            if page in ['首页','二页','三页','四页']:
-                result[group]['五页'] += num
-        for page, num in page_num.items():
-            if page in ['首页','二页','三页']:
-                result[group]['四页'] += num
-        for page, num in page_num.items():
-            if page in ['首页','二页',]:
-                result[group]['三页'] += num
-        for page, num in page_num.items():
-            if page in ['首页',]:
-                result[group]['二页'] += num
-
+    # 根据bdpc1_page5_info.txt数据计算结果
+    result_last = get_result(file_path,result,result_last)
     # 写入txt文件
     with open('{0}bdpc1_page5.txt'.format(today), 'w', encoding="utf-8") as f:
-        for group,data_dict in result.items():
+        for group,data_dict in result_last.items():
             for page,value in data_dict.items():
                 f.write(group + '\t' + page + '\t' + str(value) + '\n')
-
-    #写入excel文件
-    wb = Workbook()
-    # 创建sheet
-    for group in group_list:
-        sheet_num = 0
-        wb.create_sheet(u'{0}'.format(group),index=sheet_num)
-        sheet_num += 1
-    # 写内容
-    page_list.insert(0,'日期')
-    for group,data_dict in result.items():
-            # 写第一行日期+首页到五页后
-            wb[group].append(page_list)
-            row_value = [today]
-            # 写第二行日期+值
-            for page,value in data_dict.items():
-                row_value.append(value)
-            wb[u'{0}'.format(group)].append(row_value)
-            # 干掉日期单元格的数据
-            wb[u'{0}'.format(group)].cell(row=1,column=1,value="")
-    wb.save('{0}bdpc1_page5.xlsx'.format(today))
     end = time.time()
-    print('\n关键词共{0}个,查询任务{1}个,耗时{2}min'.format(all_num, success_num, (end - start) / 60))
+    # 写入excel
+    write_excel(group_list,result_last,today)
+    print('关键词共{0}个,成功个数看page5_info.txt,耗时{1}min'.format(all_num, (end - start) / 60))

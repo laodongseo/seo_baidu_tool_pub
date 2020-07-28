73ce019fe153df95cda1f10af31a9a673b2ca524
董董 <772483200@qq.com>
2020-07-28 14:31:17 +0800

    Update bdmo1_index_selenium_multi_monitor.py

--- bdmo1_index_selenium_multi_monitor.py
+++ bdmo1_index_selenium_multi_monitor.py
@@ -2,24 +2,29 @@
 """
 功能:
    1)指定几个域名,分关键词种类监控首页词数
-   2)抓取serp所有url,提取域名并统计各域名首页覆盖率
-   3)通过tpl属性记录serp排名url的特征
+   2)采集serp所有url,提取域名并统计各域名首页覆盖率
+   3)采集了serp上的排名url特征srcid值
 提示:
-  1)含自然排名和百度开放平台的排名
-  2)百度开放平台的样式mu属性值为排名url,mu不存在提取article里的url
-  3)2020xiaoqu_kwd_city_new.xlsx:sheet名为关键词种类,sheet第一列(A列)放关键词
+  1)相关网站.相关企业.智能小程序.其他人还在搜.热议聚合.资讯聚合.搜索智能聚合.视频全部算在内
+    所以首页排名有可能大于10
+  2)serp上自然排名mu属性值为排名url,特殊样式mu为空或不存在,
+    提取article里url,该url是baidu域名,二次访问才能获得真实url,本脚本直接取baidu链接
+  3)2020kwd_url_core_city_unique.xlsx:sheet名为关键词种类,sheet第一列(A列)放关键词
 结果:
-    bdpc1_index_info.txt:各监控站点词的排名及url,如有2个url排名,只取第一个
-    bdpc1_index_all.txt:serp所有url及样式特征,依此统计各域名首页覆盖率-单写脚本统计
-    bdpc1_index.xlsx:自己站每类词首页词数
-    bdpc1_index_domains.xlsx:各监控站点每类词的首页词数
-    bdpc1_index_domains.txt:各监控站点每类词的首页词数
+    bdmo1_index_info.txt:各监控站点词的排名及url,如有2个url排名,只取第一个
+    bdmo1_index_all.txt:serp所有url及样式特征,依此统计各域名首页覆盖率-单写脚本完成
+    bdmo1_index.xlsx:自己站每类词首页词数
+    bdmo1_index_domains.xlsx:各监控站点每类词的首页词数
+    bdmo1_index_domains.txt:各监控站点每类词的首页词数
+header头信息复制浏览器全部的请求头,Accept-Encoding留deflate
+serp上请求头多测几个找到和手动搜索结果最一致的
 """
 
 from pyquery import PyQuery as pq
 import threading
 import queue
 import time
+import json
 from urllib.parse import urlparse
 from openpyxl import load_workbook
 from openpyxl import Workbook
@@ -36,7 +41,6 @@ from selenium.webdriver.chrome.options import Options
 
 requests.packages.urllib3.disable_warnings()
 
-
 # 计算最终结果
 def get_result(file_path, result):
     for line in open(file_path, 'r', encoding='utf-8'):
@@ -52,18 +56,18 @@ def get_result(file_path, result):
 
 # 写txt,所有监控域名的结果
 def write_domains_txt(result_last):
-    with open('{0}bdpc1_index_domains.txt'.format(today), 'w', encoding="utf-8") as f_res:
-        f_res.write('{0}\t{1}\t{2}\t{3}\t{4}\n'.format('日期', '域名', '词类', '首页词数', '查询词数'))
-        for now_domain, dict_value in result_last.items():
+    with open('{0}bdmo1_index_domains.txt'.format(today), 'w', encoding="utf-8") as f_res:
+        f_res.write('{0}\t{1}\t{2}\t{3}\t{4}\n'.format('日期','域名','词类','首页词数','查询词数'))
+        for now_domain,dict_value in result_last.items():
             for group, dict_index_all in dict_value.items():
-                f_res.write('{0}\t{1}\t{2}\t'.format(today, now_domain, group))
+                f_res.write('{0}\t{1}\t{2}\t'.format(today,now_domain,group))
                 for key, value in dict_index_all.items():
                     f_res.write(str(value) + '\t')
                 f_res.write('\n')
 
 
 # 写excel
-def write_myexcel(group_list, result_last, today, my_domain):
+def write_myexcel(group_list, result_last, today,my_domain):
     wb = Workbook()
     wb_all = Workbook()
     # 创建sheet写表头
@@ -72,7 +76,7 @@ def write_myexcel(group_list, result_last, today, my_domain):
         wb.create_sheet(u'{0}'.format(group), index=sheet_num)
         wb_all.create_sheet(u'{0}'.format(group), index=sheet_num)
         row_first = ['日期', '首页', '总词数']
-        row_first2 = ['日期', '域名', '首页', '总词数']
+        row_first2 = ['日期', '域名','首页', '总词数']
         # 写表头
         wb[group].append(row_first)
         wb_all[group].append(row_first2)
@@ -83,31 +87,35 @@ def write_myexcel(group_list, result_last, today, my_domain):
             for group, dict_index_all in dict_value.items():
                 # 写数据
                 row_value = [today]
-                for key, value in dict_index_all.items():
+                for key,value in dict_index_all.items():
                     row_value.append(value)
                 wb[u'{0}'.format(group)].append(row_value)
 
         for group, dict_index_all in dict_value.items():
             # 写数据
-            row_value = [today, domain]
+            row_value = [today,domain]
             for key, value in dict_index_all.items():
                 row_value.append(value)
             wb_all[u'{0}'.format(group)].append(row_value)
-    wb.save('{0}bdpc1_index.xlsx'.format(today))
-    wb_all.save('{0}bdpc1_index_domains.xlsx'.format(today))
+    wb.save('{0}bdmo1_index.xlsx'.format(today))
+    wb_all.save('{0}bdmo1_index_domains.xlsx'.format(today))
 
 
-def get_driver():
+def  get_driver():
     option = Options()
     option.binary_location = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe"  # 安装的位置
     option.add_argument('disable-infobars')
     option.add_argument('headless')
     No_Image_loading = {"profile.managed_default_content_settings.images": 2}
+    iphone_ua = 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'
+    mobile_emulation = {"deviceMetrics": {"width": 375, "height": 667,'pixelRatio':3},"userAgent": iphone_ua}
+    option.add_experimental_option('mobileEmulation',mobile_emulation)
     option.add_experimental_option("prefs", No_Image_loading)
     driver = webdriver.Chrome(options=option, chrome_options=option)
     return driver
 
-class bdpcIndexMonitor(threading.Thread):
+class bdmoIndexMonitor(threading.Thread):
+
     def __init__(self):
         threading.Thread.__init__(self)
 
@@ -120,13 +128,13 @@ class bdpcIndexMonitor(threading.Thread):
         for sheet_obj in wb_kwd:
             sheet_name = sheet_obj.title
             group_list.append(sheet_name)
-            kwd_dict[sheet_name] = []
+            kwd_dict[sheet_name]= []
             col_a = sheet_obj['A']
             for cell in col_a:
                 kwd = (cell.value)
                 # 加个判断吧
                 if kwd:
-                    q.put([sheet_name, kwd])
+                    q.put([sheet_name,kwd])
         return q, group_list
 
     # 初始化结果字典
@@ -136,147 +144,130 @@ class bdpcIndexMonitor(threading.Thread):
         for domain in domains:
             result[domain] = {}
             for group in group_list:
-                result[domain][group] = {'首页': 0, '总词数': 0}
+                result[domain][group] = {'首页':0,'总词数':0}
         print("结果字典init...")
         return result
 
 
-    # 获取某词serp源码所有url
-    def get_encrpt_urls(self, html, url):
-        encrypt_url_list = []
-        real_urls = []
+    # 获取某词的serp源码上包含排名url的div块
+    def get_divs(self, html ,url):
+        div_list = []
         doc = pq(html)
         title = doc('title').text()
-        if '_百度搜索' in title and 'https://www.baidu.com/' in url:
-            div_list = doc('.result').items()  # 自然排名
-            div_op_list = doc('.result-op').items()  # 非自然排名
+        if '- 百度' in title and 'https://m.baidu.com/s' in url:
+            try:
+                div_list = doc('.c-result').items()
+            except Exception as e:
+                print('提取div块失败', e)
+            else:
+                pass
+        else:
+            print('源码异常---------------------',title)
+            time.sleep(120)
+        return div_list
+
+    # 提取排名的真实url
+    def get_real_urls(self, div_list):
+        real_urls_rank = []
+        if div_list:
             for div in div_list:
-                rank = div.attr('id')
-                tpl = div.attr('tpl') if div.attr('tpl') else 'xxx'
-                if rank:
-                    try:
-                        a = div('h3 a')
-                    except Exception as e:
-                        print('未提取自然排名加密链接')
+                try:
+                    data_log = div.attr('data-log')
+                    data_log = json.loads(data_log.replace("'", '"')) # json字符串双引号
+                    srcid = data_log['ensrcid'] if 'ensrcid' in data_log  else 'ensrcid' # 样式特征
+                    rank_url = data_log['mu']  if 'mu' in data_log else None # mu可能为空或者不存在
+                    rank = data_log['order']
+                except Exception as e:
+                    print('提取rank_url error',e)
+                else:
+                    if rank_url:
+                        real_urls_rank.append((rank_url,rank,srcid))
+                    # 如果mu为空或者不存在
                     else:
-                        encrypt_url = a.attr('href')
-                        encrypt_url_list.append((encrypt_url, rank, tpl))
-            for div in div_op_list:
-                rank_op = div.attr('id')
-                tpl = div.attr('tpl') if div.attr('tpl') else 'xxx'
-                if rank_op:
-                    link = div.attr('mu')  # 真实url,有些op样式没有mu属性
-                    # print(link,rank_op)
-                    if link:
-                        real_urls.append((link, rank_op, tpl))
-                    else:
-                        encrypt_url = div('article a').attr('href')
-                        encrypt_url_list.append((encrypt_url, rank_op, tpl))
-
-        else:
-            print('源码异常,可能反爬',title)
-            time.sleep(60)
-
-        return encrypt_url_list, real_urls
-
-    # 解密某条加密url
-    def decrypt_url(self, encrypt_url, my_header, retry=1):
-        real_url = None  # 默认None
-        try:
-            encrypt_url = encrypt_url.replace('http://', 'https://')
-            r = requests.head(encrypt_url, headers=my_header)
-        except Exception as e:
-            print(encrypt_url, '解密失败', e)
-            time.sleep(30)
-            if retry > 0:
-                self.decrypt_url(encrypt_url, my_header, retry - 1)
-        else:
-            real_url = r.headers['Location'] if 'Location' in r.headers else None
-        return real_url
-
-    # 获取某词serp源码首页排名真实url
-    def get_real_urls(self, encrypt_url_list):
-        real_url_list = [self.decrypt_url(encrypt_url) for encrypt_url in encrypt_url_list]
-        real_url_set = set(real_url_list)
-        real_url_set = real_url_set.remove(None) if None in real_url_set else real_url_set
-        real_url_list = list(real_url_set)
-        return real_url_list
+                        # 提取资讯聚合,图片聚合
+                        article = div('.c-result-content article')
+                        link = article.attr('rl-link-href')
+                        # 提取热议聚合
+                        if not link:
+                            a = div('.c-result-content article header a')
+                            data_log_ugc = a.attr('data-log')
+                            data_log_ugc = json.loads(data_log_ugc.replace("'", '"')) # json字符串双引号
+                            link = data_log_ugc['mu']  if 'mu' in data_log_ugc else None # mu可能为空或者不存在
+                            link = 'https://m.baidu.com{0}'.format(link) if link != None else None
+                            # 一般为卡片样式,链接太多,不提取了
+                            if not link:
+                                pass
+                        real_urls_rank.append((link,rank,srcid))
+        return real_urls_rank
 
     # 提取某url的域名部分
-    def get_domain(self, real_url):
+    def get_domain(self,real_url):
         domain = None
         try:
-            res = urlparse(real_url)
+           res = urlparse(real_url)
         except Exception as e:
-            print(e, real_url)
+           print(e,'real_url:error')
         else:
-            domain = res.netloc
+           domain = res.netloc
         return domain
 
     # 获取某词serp源码首页排名所有域名
-    def get_domains(self, real_url_list):
-        domain_list = [self.get_domain(real_url) for real_url in real_url_list]
-        # 一个词某域名多个url有排名,计算一次
-        domain_set = set(domain_list)
-        domain_set = domain_set.remove(None) if None in domain_set else domain_set
-        domain_str = ','.join(domain_set)
-        return domain_str
+    def get_domains(self,real_url_list):
+            domain_list = [self.get_domain(real_url) for real_url in real_url_list]
+            # 一个词某域名多个url有排名,算一次
+            domain_set = set(domain_list)
+            domain_set = domain_set.remove(None) if None in domain_set else domain_set
+            domain_str = ','.join(domain_set)
+            return domain_str
 
     # 线程函数
     def run(self):
         global driver
         while 1:
             group_kwd = q.get()
-            group, kwd = group_kwd
-            print(group, kwd)
-            my_header = get_header()
+            group,kwd = group_kwd
+            print(group,kwd)
             try:
-                driver.get('https://www.baidu.com/')
+                driver.get('https://m.baidu.com/')
                 input = WebDriverWait(driver, 30).until(
-                    EC.visibility_of_element_located((By.ID, "kw"))
+                    EC.visibility_of_element_located((By.ID, "index-kw"))
                 )
-                input_js = 'document.getElementById("kw").value="{0}"'.format(kwd)
+                input_js = 'document.getElementById("index-kw").value="{0}"'.format(kwd)
                 driver.execute_script(input_js) # 输入搜索词
                 baidu = WebDriverWait(driver, 20).until(
-                    EC.visibility_of_element_located((By.ID, "su"))
+                    EC.visibility_of_element_located((By.ID, "index-bn"))
                 )
-                click_js = 'document.getElementById("su").click()'
+                click_js = 'document.getElementById("index-bn").click()'
                 driver.execute_script(click_js) # 点击搜索
                 # 等待首页元素加载完毕
-                next_page = WebDriverWait(driver, 20).until(
-                    EC.visibility_of_element_located((By.ID, "help"))
+                bottom = WebDriverWait(driver, 20).until(
+                    EC.visibility_of_element_located((By.ID, "copyright"))
                 )
                 # 页面下拉
                 driver.execute_script(js_xiala)
                 html = driver.page_source
                 now_url = driver.current_url
-                encrypt_url_list_rank, real_urls_rank = self.get_encrpt_urls(html, now_url)
+                divs_res = self.get_divs(html,now_url)
                 # 源码ok再写入
-                if encrypt_url_list_rank:
-                    for my_serp_url, my_order, tpl in encrypt_url_list_rank:
-                        my_real_url = self.decrypt_url(my_serp_url, my_header)
-                        time.sleep(0.05)
-                        real_urls_rank.append((my_real_url, my_order, tpl))
+                if divs_res:
+                    real_urls_rank = self.get_real_urls(divs_res)
                     real_urls = []
-                    for my_real_url, my_order, tpl in real_urls_rank:
-                        real_urls.append(my_real_url)
-                        # print(tpl)
-                        f_all.write('{0}\t{1}\t{2}\t{3}\t{4}\n'.format(kwd, my_real_url, my_order, tpl, group))
+                    for my_url,my_order,my_attr in real_urls_rank:
+                        real_urls.append(my_url)
+                        f_all.write('{0}\t{1}\t{2}\t{3}\t{4}\n'.format(kwd,my_url,my_order,my_attr,group))
+                    f_all.flush()
                     domain_str = self.get_domains(real_urls)
                     # 目标站点是否出现
                     for domain in domains:
                         if domain not in domain_str:
-                            f.write('{0}\t{1}\t{2}\t{3}\t{4}\n'.format(kwd, '无', '无', group, domain))
+                              f.write('{0}\t{1}\t{2}\t{3}\t{4}\n'.format(kwd, '无', '无', group,domain))
                         else:
-                            for my_url, my_order, tpl in real_urls_rank:
+                            for my_url,my_order,my_attr in real_urls_rank:
                                 if domain in my_url:
-                                    f.write(
-                                        '{0}\t{1}\t{2}\t{3}\t{4}\t{5}\n'.format(kwd, my_url, my_order, group, domain,
-                                                                                tpl))
-                                    # print(my_url, my_order)
-                                    break  # 取第一个排名url
+                                    f.write('{0}\t{1}\t{2}\t{3}\t{4}\t{5}\n'.format(kwd,my_url,my_order,group,domain,my_attr))
+                                    print(my_url, my_order)
+                                    break # 取第一个排名url
                 f.flush()
-                f_all.flush()
             except Exception as e:
                 print(e)
                 driver.quit()
@@ -289,41 +280,40 @@ class bdpcIndexMonitor(threading.Thread):
                 del kwd
                 gc.collect()
                 q.task_done()
-                # time.sleep(1.5)
-
+                
 
 if __name__ == "__main__":
     start = time.time()
     local_time = time.localtime()
-    today = time.strftime('%Y%m%d', local_time)
-    domains = ['5i5j.com', 'lianjia.com', 'anjuke.com', 'fang.com']  # 目标域名
-    my_domain = '5i5j.com'
+    today = time.strftime('%Y%m%d',local_time)
+    domains = ['5i5j.com','lianjia.com','anjuke.com','fang.com'] # 目标域名
+    my_domain = '5i5j.com' # 自己域名
+    js_xiala = 'window.scrollBy(0,{0} * {1})'.format('document.body.scrollHeight',random.random())
     driver = get_driver()
-    js_xiala = 'window.scrollBy(0,{0} * {1})'.format('document.body.scrollHeight',random.random()/5)
-
-    q, group_list = bdpcIndexMonitor.read_excel('linshi.xlsx')  # 关键词队列及分类
-    result = bdpcIndexMonitor.result_init(group_list)  # 结果字典
-    # print(result)
-    all_num = q.qsize()  # 总词数
-    f = open('{0}bdpc1_index_info.txt'.format(today), 'w', encoding="utf-8")
-    f_all = open('{0}bdpc1_index_all.txt'.format(today), 'w', encoding="utf-8")
+    
+    
+    q,group_list = bdmoIndexMonitor.read_excel('2020kwd_url_core_city_unique.xlsx')  # 关键词队列及分类
+    result = bdmoIndexMonitor.result_init(group_list)  # 初始化结果
+    all_num = q.qsize() # 总词数
+    f = open('{0}bdmo1_index_info.txt'.format(today),'w',encoding="utf-8")
+    f_all = open('{0}bdmo1_index_all.txt'.format(today),'w',encoding="utf-8")
     file_path = f.name
     # 设置线程数
     for i in list(range(1)):
-        t = bdpcIndexMonitor()
+        t = bdmoIndexMonitor()
         t.setDaemon(True)
         t.start()
     q.join()
     f.close()
     f_all.close()
-    # 根据bdpc1_index_info.txt计算结果
-    result_last = get_result(file_path, result)
+    # 根据bdmo1_index_info.txt计算结果
+    result_last = get_result(file_path,result)
     # 写入txt文件
     write_domains_txt(result_last)
     # 写入excel
-    write_myexcel(group_list, result_last, today, my_domain)
-    # 统计查询成功的词数
-    with open(file_path, 'r', encoding='utf-8') as fp:
-        success = int(sum(1 for x in fp) / len(domains))
+    write_myexcel(group_list,result_last,today,my_domain)
+     # 统计查询成功的词数
+    with open(file_path,'r',encoding='utf-8') as fp:
+         success = int(sum(1 for x in fp)/len(domains))
     end = time.time()
-    print('关键词共{0}个,查询成功{1}个,耗时{2}min'.format(all_num, success, (end - start) / 60))
+    print('关键词共{0}个,耗时{1}min'.format(all_num, (end - start) / 60))

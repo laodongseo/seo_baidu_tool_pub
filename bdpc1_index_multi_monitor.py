f6a8f80a833b101b5bdc785f639ca98f03cfdb69
董董 <772483200@qq.com>
2020-10-27 16:35:39 +0800

    Update bdpc1_index_multi_monitor.py

--- bdpc1_index_multi_monitor.py
+++ bdpc1_index_multi_monitor.py
@@ -219,7 +219,7 @@ class bdpcIndexMonitor(threading.Thread):
         real_url = None # 默认None
         try:
             encrypt_url = encrypt_url.replace('http://','https://')
-            r = requests.head(encrypt_url,headers=my_header)
+            r = requests.head(encrypt_url,headers=my_header,timeout=10)
         except Exception as e:
             print(encrypt_url,'解密失败',e)
             time.sleep(30)

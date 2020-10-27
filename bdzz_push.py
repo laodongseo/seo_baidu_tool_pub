839efeae5c278cebdc91680b7a1f790411a281eb
董董 <772483200@qq.com>
2020-10-27 16:51:49 +0800

    Update bdzz_push.py

--- bdzz_push.py
+++ bdzz_push.py
@@ -2,12 +2,11 @@
 import requests
 import time
 """
-主动推送脚本
-多站点配置文件conf.txt 格式如下 一行一个
+多站点配置文件conf.txt 格式如下
 qd.xxx.com=tuz16xxxxNuxa2E=qd.txt
 sh.xxx.com=tuz16xxxxNuxa3E=sh.txt
-qd.txt存放qd的url
-sh.txt存放sh的url
+qd.txt存放qd的url 一行一个
+sh.txt存放sh的url 一行一个
 每个域名用完配额以后会进行下一个域名的推送
 """
 
@@ -30,38 +29,39 @@ def read_txt(filepath):
 
 
 # 推送url函数
-def push_url(retry=0):
+def push_url(domain,retry=1):
             if  all_urls == []:
-                return '结束'
+                return '下一个'
             for data_urls in all_urls:
                 try:
                     str_urls = ''.join(data_urls)
                     r = requests.post(post_url,data=str_urls,headers=user_agent,timeout=5)
                 except Exception as e:
-                    print('获取源码失败',e)
+                    print('请求失败,暂停15秒',e)
                     time.sleep(15)
                     if retry > 0:
                         push_url(retry-1)
-                finally:
-                    try:
-                        html = r.text
-                    except Exception as e:
-                        html = ''
-                    finally:
-                        if '"success"' in html:
-                            print(html,'推送成功',len(data_urls))
-                            
-                        elif '"error"' in html and 'over quota' in html:
-                            return '结束'
-                        else:
-                            f.write(str_urls)
-                            f.flush()
-                            print('推送失败，保存到文件')
+                else:
+                    html = r.json()
+                    keys = [ key for key,value in html.items()]
+                    values = [ value for key,value in html.items()]
+                    if 'success' in keys:
+                        print(domain,'推送成功',len(data_urls))
+                    elif 'error' in keys and 'over quota' in values:
+                        print(domain,'配额不够,开始下一个域名推送',html)
+                        return '下一个'
+                    elif 'error' in keys and 'site error' in values:
+                        print(domain,'站点配置错误,略过',html)
+                        return '下一个'
+                    else:
+                        f.write(str_urls)
+                        f.flush()
+                        print(domain,'推送失败，保存到文件',html)
 
 
 
 if __name__ == "__main__":
-    f = open('push_fail.txt','w',encoding='utf-8')
+    f = open('push_fail.txt','w',encoding='utf-8') # 推送失败的url
     user_agent = {
         'User - Agent':'curl / 7.12.1',
         'Host':'data.zz.baidu.com',
@@ -73,17 +73,16 @@ if __name__ == "__main__":
     for line in configs:
         lis = line.split('=')
         domain = lis[0]
-        print(domain)
+        print('开始推送',domain)
         token = lis[1]
         file_txt = lis[2]
         all_urls = read_txt(file_txt)  # 二维列表每组2000个url
         post_url = 'http://data.zz.baidu.com/urls?site=https://{0}&token={1}'.format(domain, token)
-        
         # 死循环每个域名把配额用完
         while 1:
-            time.sleep(3)
-            res = push_url()
-            if '结束' == res:
+            time.sleep(1)
+            res = push_url(domain)
+            if '下一个' == res:
                 break
 
     f.close()

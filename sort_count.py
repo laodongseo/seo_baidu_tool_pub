67c38fa16bd979e2f8815aa18771d2f2e98594a9
董董 <772483200@qq.com>
2023-08-24 14:18:54 +0800

    Add files via upload

--- sort_count.py
+++ sort_count.py
@@ -1,4 +1,10 @@
 # ‐*‐ coding: utf‐8 ‐*‐
+"""
+读取文件关键词
+然后排序
+"""
+import os 
+
 
 # 获取文件的词
 def get_kwds(filepath):
@@ -8,16 +14,19 @@ def get_kwds(filepath):
         dicts_kwd[kwd] = dicts_kwd[kwd] + 1 if kwd in dicts_kwd else 1
     return dicts_kwd
 
+
 # 结果排序
 def sort_dict(dicts_kwd):
     res = sorted(dicts_kwd.items(), key=lambda e:e[1], reverse=True)
     for kwd,count in res:
-        f_count.write('{0}\t{1}\n'.format(kwd,count))
+        f_count.write(f'{kwd}\t{count}\n')
         f_count.flush()
 
+
 if __name__ == "__main__":
-    base_file  = 'bdpc_xiala'
-    f_count = open(f'{base_file}_sort.txt','w',encoding='utf-8')
-    dict_kwds = get_kwds(f'{base_file}.txt')
+    base_file  = 'kwd_serpkwds20230727.txt'
+    res_file = os.path.splitext(base_file)[0] + 'sort.txt'
+    f_count = open(res_file,'w',encoding='utf-8')
+    dict_kwds = get_kwds(base_file)
     sort_dict(dict_kwds)
     f_count.close()
